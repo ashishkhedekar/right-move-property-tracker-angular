04a10d37a8f9c18b1adbc592009964fcdb84b10c
Ashish Khedekar <ashish.khedekar@bmigroup.com>
2021-04-04 13:19:05 +0100

    Styling and some images

--- src/app/components/properties-list/properties-list.component.ts
+++ src/app/components/properties-list/properties-list.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {LocationDetailsInterface, LocationsService} from '../../services/locations.service';
-import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
+import {NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-properties-list',
@@ -12,20 +12,17 @@ export class PropertiesListComponent implements OnInit, AfterViewInit {
 
   @Input() locationId: string;
   @Input() channel: string;
-  displayedColumns: string[] = ['mainMapImageSrc', 'displayAddress', 'bedrooms', 'displayPrice', 'firstVisibleDate', 'offMarketDate', 'daysOnMarket'];
   properties: LocationDetailsInterface[] = [];
-  propertiesDataSource: MatTableDataSource<LocationDetailsInterface> = new MatTableDataSource();
   @ViewChild(MatSort) sort: MatSort;
-
+  offMarketInDays = [];
   constructor(private locationsService: LocationsService) { }
 
   ngAfterViewInit() {
-    this.propertiesDataSource.sort = this.sort;
+    this.offMarketInDays = ['1', '2'];
   }
 
   ngOnInit(): void {
     this.locationsService.loadLocationMarketDetails(this.locationId).subscribe(locationDetails => {
-      console.log(`Found ${locationDetails.length} properties`);
       locationDetails.forEach(locationDetail => {
         if (locationDetail.channel === this.channel)
         {
@@ -36,12 +33,23 @@ export class PropertiesListComponent implements OnInit, AfterViewInit {
           }
         }
       });
-      this.propertiesDataSource.data = this.properties;
-
-
-
-
-
     });
   }
+
+  handleChange(typeOfProperties){
+    this.properties = [];
+    this.locationsService.loadLocationMarketDetails(this.locationId).subscribe(locationDetails => {
+      locationDetails.forEach(locationDetail => {
+        if (locationDetail.channel === typeOfProperties)
+        {
+          this.properties.push(locationDetail);
+          if (locationDetail.mainMapImageSrc === null || locationDetail.mainMapImageSrc === '')
+          {
+            locationDetail.mainMapImageSrc = 'https://specialistbuildingsupplies.co.uk/wp-content/uploads/2021/02/image-coming-soon-placeholder.png';
+          }
+        }
+      });
+    });
+  }
+
 }
