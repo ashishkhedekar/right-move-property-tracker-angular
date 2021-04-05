3af8646137a74ac09c60f428c7074a21d7d6128f
Ashish Khedekar <ashish.khedekar@bmigroup.com>
2021-04-05 19:58:10 +0100

    Some stylesheets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,17 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'propertytracker-angular';
+  public expand = false;
+
   constructor() {
+  }
+
+  toggle()
+  {
+    console.log('Menu to be showed before' + this.expand);
+    this.expand = !this.expand;
+    console.log('Menu to be showed after' + this.expand);
   }
 }
